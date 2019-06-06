acdcfcd7e93b11ea66d3e079d8ef4929be94b6d0
João Pedro Sá Medeiros <jpsmedeiros@id.uff.br>
2019-06-06 00:26:59 -0300

    Medindo tempo de execução

--- src/__main__.py
+++ src/__main__.py
@@ -1,15 +1,18 @@
 from model import Model
 from solver import Solver
+import time
 
 def main():
     dimensionality = (2,2)
     nx = 0.15
     ny = 0.15
     delta_t = 0.1
+    start_time = time.time()
     model = Model(nx, ny, dimensionality)
     solver = Solver(model, delta_t)
     solver.solve()
-
+    end_time = time.time()
+    print(end_time - start_time)
 
 if __name__ == "__main__":
     main()
